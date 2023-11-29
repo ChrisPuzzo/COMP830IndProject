bf8da43b63de8d21b31c4d067e1288e15d2c2bec
ChrisPuzzo <cpp1015@wildcats.unh.edu>
2023-11-29 13:48:22 -0500

    Added in the framework for the game

---
+++ main.py
@@ -0,0 +1,36 @@
+
+from ast import main
+
+
+class Game:
+    
+    questionsDict = {}
+
+
+    def answerYes(self, nextQuestion):
+        pass
+
+    def answerNo(self, nextQuestion):
+        pass
+
+    def showQuestion(self, question, nextY, nextN):
+        print(question "\n")
+        print("(1)Yes or (2)No?: ")
+        ans = input()
+        if ans == "1":
+            self.answerYes(nextY)
+
+        if ans == "2":
+            self.answerNo(nextN)
+
+        else:
+            print("Please type either 1 or 2")                    
+
+    def gameStart():
+        print("the game has started")
+
+
+
+runGame = Game
+
+runGame.gameStart()
