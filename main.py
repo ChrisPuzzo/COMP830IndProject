39670cef6d5994b3fa52385e74689bbbaffbede6
ChrisPuzzo <cpp1015@wildcats.unh.edu>
2023-12-18 13:32:42 -0500

    Added in all the questions

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ class Game:
                 "1":["Welcome to the game! Think of a design pattern and answer these following yes/no question?", "2", "stop"],
                 "2":["Does it provide the oject creation mechanisms that enhance the flexabilities of the existing codes?", "3", "4"],
                 "3":["Does it ensure that you have at most one instance of a class in your application?", "5", "6",],
-                "4":["Is it respobsible for how classes communicate with each other?"],
                 "5":["is it the Singleton Design Pattern?", "done", "wrong"],
                 "6":["is it the Builder Design Pattern?", "done", "wrong"],
-                
+                "4":["Is it respobsible for how classes communicate with each others?", "7", "8"],
+                "7":["Does it provide a mechanism to the context to change it's behavior?","9", "10"],
+                "9":["Is changing behavior built into it's scheme?", "11", "12"],
+                "10":["Does it allow a group of objects to be notified when some state changes? ", "13", "14"],
+                "11":["Is it the state pattern?", "done", "wrong"],
+                "12":["Is it the strategy pattern?", "done", "wrong"],
+                "13":["Is it the Observer Pattern?", "done", "wrong"],
+                "14":["Is it the Commander Pattern", "done", "wrong"],
+                "8":["Does it explain how to assemble objects and classes into a larger structure and simplifies the structure by indentifiying the relationship", "15", "stop"],
+                "15":["Does it attach additional behavior to an object dynamically at run time", "16", "17"],
+                "16":["is it the Decorator Pattern?", "done", "wrong"],
+                "17":["Is it the Adapter Pattern?", "done", "wrong"]
             }
     
     def answerYes(self, nextQuestion):
